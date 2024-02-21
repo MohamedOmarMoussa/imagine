d41c81ffc8fbb10189beced2aa527fb6d1458491
Mohamed Omar Moussa <mohamedomarmoussa02@gmail.com>
2024-02-21 18:30:30 +0100

    mobile-navigation-ok

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,8 @@
-import { UserButton } from "@clerk/nextjs"
 
 const Home = () => {
   return (
     <div>
       <p>Home</p>
-      <UserButton />
     </div>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const IBMPlex = IBM_Plex_Sans({
 });
 
 export const metadata: Metadata = {
-  title: "Imagine",
+  title: "Imaginify",
   description: "AI-powered image generator",
 };
 
