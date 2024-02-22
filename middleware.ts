639cf10fcb4bbf99e9e14ab6d0ff1023c12dfa80
Mohamed Omar Moussa <mohamedomarmoussa02@gmail.com>
2024-02-22 20:14:22 +0100

    server-actions

--- middleware.ts
+++ middleware.ts
@@ -3,9 +3,6 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
 
   publicRoutes: ['/api/webhooks/clerk'],
-  // Routes that can always be accessed, and have
-  // no authentication information
-  ignoredRoutes: ['/no-auth-in-this-route'],
 });
 
 export const config = {
