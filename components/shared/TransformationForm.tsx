b048ca925b0fd0510c07e8b42cdbbaf07849211e
Mohamed Omar Moussa <mohamedomarmoussa02@gmail.com>
2024-02-23 15:12:15 +0100

    form_1_ok

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -1,8 +1,101 @@
-import React from 'react'
+"use client"
 
-const TransformationForm = () => {
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { defaultValues, transformationTypes } from "@/constants"
+import { CustomField } from "./CustomField"
+import { useState } from "react"
+
+export const formSchema = z.object({
+  title: z.string(),
+  aspectRatio: z.string().optional(),
+  color: z.string().optional(),
+  prompt: z.string().optional(),
+  publicId: z.string(),
+})
+
+const TransformationForm = ({ action, data = null, userId, type, creditBalance }: TransformationFormProps) => {
+  const transformationType = transformationTypes[type];
+  const [image, setImage] = useState(data);
+  const [newTransformation, setNewTransformation] = useState<Transformations | null>(null);
+
+  const initialValues = data && action === "Update" ? {
+    title: data?.title,
+    aspectRatio: data?.aspectRatio,
+    color: data?.color,
+    prompt: data?.prompt,
+    publicId: data?.publicId,
+  } : defaultValues
+
+  // 1. Define your form
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialValues,
+  })
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+
+  const onSelectFieldHandler = (value: string, onChangeField: (value: string) => void) => {
+
+  }
   return (
-    <div>TransformationForm</div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <CustomField
+          control={form.control}
+          name="title"
+          formLabel="Aspect Ratio"
+          className="w-full"
+          render={({ field }) => <Input {...field}
+          className="input-field0" />}
+        />
+
+        {type === "fill" && (
+          <CustomField
+            control={form.control}
+            name="aspectRatio"
+            formLabel="Aspect Ratio"
+            className="w-full"
+            render={({ field }) => (
+              <Select>
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder="Theme" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="light">Light</SelectItem>
+                  <SelectItem value="dark">Dark</SelectItem>
+                  <SelectItem value="system">System</SelectItem>
+                </SelectContent>
+              </Select>
+            )}
+          />
+        )}
+      </form>
+    </Form>
   )
 }
 
