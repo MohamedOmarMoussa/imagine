7c7e8263222b2592efb43a85c0d282f583413e52
Mohamed Omar Moussa <mohamedomarmoussa02@gmail.com>
2024-02-24 17:46:01 +0100

    cloudinary_media_uploader_ok

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,6 @@
 import MobileNav from "@/components/shared/MobileNav"
 import Sidebar from "@/components/shared/Sidebar"
-import React from "react"
+import { Toaster } from "@/components/ui/toaster"
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -12,6 +12,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
           {children}
         </div>
       </div>
+      <Toaster />
     </main>
   )
 }
