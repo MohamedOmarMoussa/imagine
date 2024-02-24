a4a3085bdef4e78bacd1862ec20e3ae988df8a14
Mohamed Omar Moussa <mohamedomarmoussa02@gmail.com>
2024-02-24 16:31:32 +0100

    react_hook&zod_validation

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -12,18 +12,22 @@ const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps)
   if(!userId) redirect("/sign-in")
 
   const user = await getUserById(userId);
+
   return (
     <>
       <Header
         title={transformation.title}
         subtitle={transformation.subTitle}
       />
-      <TransformationForm
-        action="Add"
-        userId={user._id}
-        type={transformation.type as TransformationTypeKey}
-        creditBalance={user.creditBalance}
-      />
+      
+      <section className='mt-10'>
+        <TransformationForm
+          action="Add"
+          userId={user._id}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        />
+      </section>
     </>
   )
 }
