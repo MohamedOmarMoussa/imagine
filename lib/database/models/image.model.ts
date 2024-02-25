9aa18840f677e6e74ca1f6f8cd2ebfc7058e6400
Mohamed Omar Moussa <mohamedomarmoussa02@gmail.com>
2024-02-25 15:25:29 +0100

    image_server_action&insufficient_credits_modal_done

--- lib/database/models/image.model.ts
+++ lib/database/models/image.model.ts
@@ -4,41 +4,40 @@ export interface Image extends Document {
   title: string;
   transformationType: string;
   publicId: string;
-  secureUrl: string;
+  secureURL: string;
   width?: number;
   height?: number;
   config?: object;
-  transformationUrl?: URL;
+  transformationUrl?: string;
   aspectRatio?: string;
   color?: string;
   prompt?: string;
-  auth: {
+  author: {
     _id: string;
     firstName: string;
     lastName: string;
-  };
+  }
   createdAt?: Date;
   updatedAt?: Date;
 }
-
 
 const ImageSchema = new Schema({
   title: { type: String, required: true },
   transformationType: { type: String, required: true },
   publicId: { type: String, required: true },
-  secureUrl: { type: String, required: true },
+  secureURL: { type: String, required: true },
   width: { type: Number },
   height: { type: Number },
   config: { type: Object },
-  transformationUrl: { type: URL },
+  transformationUrl: { type: String },
   aspectRatio: { type: String },
   color: { type: String },
   prompt: { type: String },
-  auth: { type: Schema.Types.ObjectId, ref: "User" },
+  author: { type: Schema.Types.ObjectId, ref: 'User' },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now }
-})
+});
 
-const Image = models?.Image || model("Image", ImageSchema);
+const Image = models?.Image || model('Image', ImageSchema);
 
 export default Image;
