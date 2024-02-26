49aafdf19fef6383729e53b08572227f00538deb
Mohamed Omar Moussa <mohamedomarmoussa02@gmail.com>
2024-02-26 18:42:13 +0100

    middleware

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
-}); 
+  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe'],
+  debug: true
+});
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
