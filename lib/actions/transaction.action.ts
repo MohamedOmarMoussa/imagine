8a123473be988af8fc59ae58f9d1fc210a104b9c
Mohamed Omar Moussa <mohamedomarmoussa02@gmail.com>
2024-02-26 15:29:18 +0100

    checkout&pay_with_stripe_done

---
+++ lib/actions/transaction.action.ts
@@ -0,0 +1,57 @@
+"use server";
+
+import Stripe from "stripe";
+import { redirect } from "next/navigation";
+import { connectToDatabase } from "../database/mongoose";
+import { handleError } from "../utils";
+import Transaction from "../database/models/transaction.model";
+import { updateCredits } from "./user.actions";
+
+export async function checkoutCredits(transaction: CheckoutTransactionParams) {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
+  const amount = Number(transaction.amount) * 100;
+
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        price_data: {
+          currency: 'usd',
+          unit_amount: amount,
+          product_data: {
+            name: transaction.plan
+          }
+        },
+        quantity: 1
+      }
+    ],
+    metadata: {
+      plan: transaction.plan,
+      credits: transaction.credits,
+      buyerId: transaction.buyerId,
+    },
+    mode: "payment",
+    success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/profile`,
+    cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
+  })
+
+  redirect(session.url!)
+}
+
+export async function createTransaction(transaction: CreateTransactionParams) {
+  try {
+    await connectToDatabase();
+
+    // Create a new transaction with buyerId
+    const newTransaction = await Transaction.create({
+      ...transaction, buyer: transaction.buyerId
+    })
+
+    await updateCredits(transaction.buyerId, transaction.credits);
+
+    return JSON.parse(JSON.stringify(newTransaction))
+
+  } catch (error) {
+    handleError(error)
+  }
+}
